e8d29f3943d852a3dede1640cd90819653bb503e
Humphryshikunzi <39647238+Humphryshikunzi@users.noreply.github.com>
2020-05-28 17:36:47 +0300

    Made changes

--- ImageUploader/MainPage.xaml.cs
+++ ImageUploader/MainPage.xaml.cs
@@ -2,12 +2,8 @@
 using Plugin.Media;
 using Plugin.Media.Abstractions;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace ImageUploader
